a630afe1e69111a57096be338a325f6eded38411
Anurag Choubey <anurag.choubey1995@gmail.com>
2025-01-28 17:48:29 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O3 -march=native -mtune=intel -funroll-loops -ftree-vectorize -ffast-math -fopenmp -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) hw3tensor.cc -o hw3tensor$(python3-config --extension-suffix) 
+g++ -O3 -march=native -mtune=intel -funroll-loops -ftree-vectorize -ffast-math -fopenmp -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) tensor.cc -o tensor$(python3-config --extension-suffix) 
